8edad454fc759ca186577d7e3bd61ef62d6ba8bb
Harnoor <g.harnoor15@gmail.com>
2020-04-17 22:15:59 +1200

    update

--- js/storage.js
+++ js/storage.js
@@ -1,38 +1,30 @@
 function upload() {
-    // Get content
-    // let uploader = document.getElementById("uploader");
+    // Get file
     let file = document.getElementById("file_button").files[0];
 
-    // Listen for file selection
-    // file_button.addEventListener('change', function (e) {
-        
-        // Get file
-        // let file = e.target.files[0];
+    // Create storage ref
+    let storage_ref = firebase.storage().ref('uploaded_audio/' + file.name);
 
-        // Create storage ref
-        let storage_ref = firebase.storage().ref('user_audio/' + file.name);
+    // Upload file
+    let task = storage_ref.put(file);
 
-        //Upload file
-        let task = storage_ref.put(file);
+    // Progress
+    task.on('state_changed', 
+        function progress(snapshot) {
+            let percent = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+            console.log("upload is " + percent +" done");
+        },
+        function error(err){
+            console.log(err.message);
+        },
+        function complete() {
+            
+        },
+        function () {task.snapshot.ref.getDownloadURL().then(
+            function (downlaodURL) {
 
-        // progress
-        task.on('state_changed', 
-            function progress(snapshot) {
-                let percent = snapshot.bytesTransferred / snapshot.totalBytes * 100;
-                console.log("upload is " + percent +" done");
-            },
-            function error(err){
-                console.log(err.message);
-            },
-            function complete() {
-                
-            },
-            function () {
-                task.snapshot.ref.getDownloadURL().then(
-                    function (downlaodURL) {
-                    //get your upload image url here...
-                    console.log(downlaodURL);
-                });
-            });
-    // });
+            // Get your upload image url 
+            console.log(downlaodURL);
+        });
+    });
 }
